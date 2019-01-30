6a5b98e58b950e0bb94b57df1786fe7becb31b81
Abhilash Sulibela <abhilash.sulilbela@mavs.uta.edu>
2019-01-30 10:47:23 -0600

    All changes (Preempted/final)

--- server/models.py
+++ server/models.py
@@ -6,224 +6,220 @@ from marshmallow import validate, fields, Schema
 import json
 from sqlalchemy.ext.hybrid import hybrid_property
 
+
 class CRUD():
-	def add(self, resource):
-		db.session.add(resource)
-		return db.session.commit()
+  def add(self, resource):
+    db.session.add(resource)
+    return db.session.commit()
 
-	def update(self):
-		return db.session.commit()
+  def update(self):
+    return db.session.commit()
 
-	def delete(self, resource):
-		db.session.delete(resource)
-		return db.session.commit()
+  def delete(self, resource):
+    db.session.delete(resource)
+    return db.session.commit()
 
 
 class User(db.Model):
-	__tablename__ = 'users'
+  __tablename__ = 'users'
 
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(255), nullable=True, default="User")
-	email = db.Column(db.String(255), index=True, unique=True, nullable=False)
-	password = db.Column(db.String(255), nullable=False)
-	registered_on = db.Column(db.DateTime, nullable=False)
-	admin = db.Column(db.Boolean, nullable=False, default=False)
-	concepts = db.relationship('Concepts', backref="users", cascade="all, delete-orphan", lazy="dynamic")
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  name = db.Column(db.String(255), nullable=True, default="User")
+  email = db.Column(db.String(255), index=True, unique=True, nullable=False)
+  password = db.Column(db.String(255), nullable=False)
+  registered_on = db.Column(db.DateTime, nullable=False)
+  admin = db.Column(db.Boolean, nullable=False, default=False)
+  concepts = db.relationship('Concepts', backref="users", cascade="all, delete-orphan", lazy="dynamic")
 
-	def __init__(self, name, email, password, admin=False):
-		self.name = name
-		self.email = email
-		self.password = bcrypt.generate_password_hash(password)
-		self.registered_on = datetime.datetime.now()
-		self.admin = admin
+  def __init__(self, name, email, password, admin=False):
+    self.name = name
+    self.email = email
+    self.password = bcrypt.generate_password_hash(password)
+    self.registered_on = datetime.datetime.now()
+    self.admin = admin
 
-	def is_authenticated(self):
-		return True
+  def is_authenticated(self):
+    return True
 
-	def is_active(self):
-		return True
+  def is_active(self):
+    return True
 
-	def is_anonymous(self):
-		return False
+  def is_anonymous(self):
+    return False
 
-	def get_id(self):
-		return self.id
+  def get_id(self):
+    return self.id
 
-	def __repr__(self):
-		return '<id {}>'.format(self.id)
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
 
 
 class Concepts(db.Model, CRUD):
-	__tablename__ = 'concepts'
+  __tablename__ = 'concepts'
 
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(255), nullable=False, unique=True)
-	help_text = db.Column(db.String(255))
-	created_on = db.Column(db.DateTime, nullable=False)
-	edited_on = db.Column(db.DateTime, nullable=False)
-	creator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	creator = db.relationship("User", back_populates="concepts")
-	creator_name = db.Column(db.String(255), nullable=False)
-	
-	concept_type=db.Column(db.String(20), default='bipolar', nullable=False)
-	input_terms=db.Column(JSON)
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  name = db.Column(db.String(255), nullable=False, unique=True)
+  help_text = db.Column(db.String(255))
+  created_on = db.Column(db.DateTime, nullable=False)
+  edited_on = db.Column(db.DateTime, nullable=False)
+  creator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  creator = db.relationship("User", back_populates="concepts")
+  creator_name = db.Column(db.String(255), nullable=False)
 
-	def __init__(self, name, creator_id, concept_type, input_terms, creator_name ):
-		self.name = name
-		self.creator_id = creator_id
-		self.concept_type = concept_type
-		self.input_terms = input_terms
-		self.created_on = datetime.datetime.now()
-		self.edited_on = datetime.datetime.now()
-		self.creator_name = creator_name
+  concept_type = db.Column(db.String(20), default='bipolar', nullable=False)
+  input_terms = db.Column(JSON)
 
-	def get_id(self):
-		return self.id
+  def __init__(self, name, creator_id, concept_type, input_terms, creator_name):
+    self.name = name
+    self.creator_id = creator_id
+    self.concept_type = concept_type
+    self.input_terms = input_terms
+    self.created_on = datetime.datetime.now()
+    self.edited_on = datetime.datetime.now()
+    self.creator_name = creator_name
 
-	def __repr__(self):
-		return '<id {}>'.format(self.id)
+  def get_id(self):
+    return self.id
+
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
+
 
 class ConceptsSchema(Schema):
-	
-	not_blank = validate.Length(min=1, error='Field cannot be blank')
-	id = fields.Integer(dump_only=False)
-	name = fields.Str()
-	created_on = fields.DateTime()
-	creator_id = fields.Integer()
-	concept_type = fields.Str()
-	input_terms = fields.Raw()
-	creator_name = fields.Str()
-	help_text = fields.Str()
-	
-	#self links
-	def get_top_level_links(self, data, many):
-		if many:
-			self_link = "/concepts/"
-		else:
-			self_link = "/concepts/{}".format(data['id'])
-		return {'self': self_link}
+  not_blank = validate.Length(min=1, error='Field cannot be blank')
+  id = fields.Integer(dump_only=False)
+  name = fields.Str()
+  created_on = fields.DateTime()
+  creator_id = fields.Integer()
+  concept_type = fields.Str()
+  input_terms = fields.Raw()
+  creator_name = fields.Str()
+  help_text = fields.Str()
 
-	class Meta:
-		type_ = 'concepts'
+  # self links
+  def get_top_level_links(self, data, many):
+    if many:
+      self_link = "/concepts/"
+    else:
+      self_link = "/concepts/{}".format(data['id'])
+    return {'self': self_link}
+
+  class Meta:
+    type_ = 'concepts'
+
 
 class Article(db.Model):
-	__tablename__ = 'articles'
+  __tablename__ = 'articles'
 
-	url = db.Column(db.String)
-	adx_keywords = db.Column(db.String)
-	column = db.Column(db.String)
-	section = db.Column(db.String)
-	byline = db.Column(db.String)
-	type= db.Column(db.String)
-	title = db.Column(db.String)
-	abstract = db.Column(db.String)
-	published_date = db.Column(db.DateTime)
-	source= db.Column(db.String)
-	id = db.Column(BIGINT, primary_key=True)
-	asset_id = db.Column(BIGINT)
-	views = db.Column(db.Integer)
-	des_facet = db.Column(ARRAY(db.String))
-	org_facet = db.Column(ARRAY(db.String))
-	per_facet = db.Column(ARRAY(db.String))
-	geo_facet = db.Column(ARRAY(db.String))
-	media = db.Column(ARRAY(db.String))
-	comments = db.relationship("Comment", back_populates="article")
+  url = db.Column(db.String)
+  adx_keywords = db.Column(db.String)
+  column = db.Column(db.String)
+  section = db.Column(db.String)
+  byline = db.Column(db.String)
+  type = db.Column(db.String)
+  title = db.Column(db.String)
+  abstract = db.Column(db.String)
+  published_date = db.Column(db.DateTime)
+  source = db.Column(db.String)
+  id = db.Column(BIGINT, primary_key=True)
+  asset_id = db.Column(BIGINT)
+  views = db.Column(db.Integer)
+  des_facet = db.Column(ARRAY(db.String))
+  org_facet = db.Column(ARRAY(db.String))
+  per_facet = db.Column(ARRAY(db.String))
+  geo_facet = db.Column(ARRAY(db.String))
+  media = db.Column(ARRAY(db.String))
+  comments = db.relationship("Comment", back_populates="article")
 
-	@hybrid_property
-	def comments_count(self):
-	    return len(self.comments)
+  @hybrid_property
+  def comments_count(self):
+    return len(self.comments)
 
-	@comments_count.expression
-	def comments_count(cls):
-		return select([func.count(Comment)]).\
-				where(Comment.asset_id==cls.id).\
-				label('num_comments')
+  @comments_count.expression
+  def comments_count(cls):
+    return select([func.count(Comment)]). \
+      where(Comment.asset_id == cls.id). \
+      label('num_comments')
 
+  def __init__(self, apiResult):
+    apiResult['media'] = [json.dumps(i) for i in apiResult['media']]
+    for key, value in apiResult.iteritems():
+      setattr(self, key, value)
 
-	def __init__(self, apiResult):
-		apiResult['media'] = [ json.dumps(i) for i in apiResult['media']]
-		for key, value in apiResult.iteritems():
-			setattr(self, key, value)
+  def __repr__(self):
+    return '<URL {}>'.format(self.url)
 
-	def __repr__(self):
-		return '<URL {}>'.format(self.url)
 
 class ArticleSchema(Schema):
-	
-	#self links
-	def get_top_level_links(self, data, many):
-		if many:
-			self_link = "/articles/"
-		else:
-			self_link = "/articles/{}".format(data['id'])
-		return {'self': self_link}
 
-	class Meta:
-		type_ = 'articles'
-		comments = fields.Nested('CommentSchema',many=True)
-		# url = fields.Str()
+  # self links
+  def get_top_level_links(self, data, many):
+    if many:
+      self_link = "/articles/"
+    else:
+      self_link = "/articles/{}".format(data['id'])
+    return {'self': self_link}
 
-		fields = ('id','url','adx_keywords','section','type','title','abstract',
-			'published_date','comments_count','byline','media', 
-			'des_facet','org_facet','per_facet','geo_facet')
+  class Meta:
+    type_ = 'articles'
+    comments = fields.Nested('CommentSchema', many=True)
+    # url = fields.Str()
 
-
+    fields = ('id', 'url', 'adx_keywords', 'section', 'type', 'title', 'abstract',
+              'published_date', 'comments_count', 'byline', 'media',
+              'des_facet', 'org_facet', 'per_facet', 'geo_facet')
 
 
 class Comment(db.Model):
-	__tablename__ = 'comments'
+  __tablename__ = 'comments'
 
-	approveDate = db.Column(db.String)
-	commentBody = db.Column(db.String)
-	commentID = db.Column(db.Integer, primary_key=True)
-	commentSequence = db.Column(db.Integer)
-	commentTitle = db.Column(db.String)
-	commentType = db.Column(db.String)
-	createDate = db.Column(db.String)
-	depth = db.Column(db.Integer)
-	editorsSelection = db.Column(db.Boolean)
-	parentID = db.Column(db.Integer, db.ForeignKey('comments.commentID'))
-	parentUserDisplayName = db.Column(db.String)
-	permID = db.Column(db.String)
-	picURL = db.Column(db.String)
-	recommendations = db.Column(db.Integer)
-	replies = db.relationship("Comment")
-	replyCount = db.Column(db.Integer)
-	reportAbuseFlag = db.Column(db.String)
-	status = db.Column(db.String)
-	trusted = db.Column(db.Integer)
-	updateDate = db.Column(db.String)
-	userID = db.Column(db.Integer)
-	userDisplayName = db.Column(db.String)
-	userTitle = db.Column(db.String)
-	userURL = db.Column(db.String)
-	userLocation = db.Column(db.String)
-	assetID = db.Column(BIGINT, db.ForeignKey('articles.id'))
-	article = db.relationship("Article", back_populates="comments")
-	
+  approveDate = db.Column(db.String)
+  commentBody = db.Column(db.String)
+  commentID = db.Column(db.Integer, primary_key=True)
+  commentSequence = db.Column(db.Integer)
+  commentTitle = db.Column(db.String)
+  commentType = db.Column(db.String)
+  createDate = db.Column(db.String)
+  depth = db.Column(db.Integer)
+  editorsSelection = db.Column(db.Boolean)
+  parentID = db.Column(db.Integer, db.ForeignKey('comments.commentID'))
+  parentUserDisplayName = db.Column(db.String)
+  permID = db.Column(db.String)
+  picURL = db.Column(db.String)
+  recommendations = db.Column(db.Integer)
+  replies = db.relationship("Comment")
+  replyCount = db.Column(db.Integer)
+  reportAbuseFlag = db.Column(db.String)
+  status = db.Column(db.String)
+  trusted = db.Column(db.Integer)
+  updateDate = db.Column(db.String)
+  userID = db.Column(db.Integer)
+  userDisplayName = db.Column(db.String)
+  userTitle = db.Column(db.String)
+  userURL = db.Column(db.String)
+  userLocation = db.Column(db.String)
+  assetID = db.Column(BIGINT, db.ForeignKey('articles.id'))
+  article = db.relationship("Article", back_populates="comments")
 
-	def __init__(self, apiResult, assetID):
-		try:
-			if 'replies' in apiResult:
-				del apiResult['replies'] 
-			self.assetID = assetID
-			for key, value in apiResult.iteritems():
-				setattr(self, key, value)
-		except Exception as e:
-			pdb.set_trace()
-			print apiResult
-			print e
+  def __init__(self, apiResult, assetID):
+    try:
+      if 'replies' in apiResult:
+        del apiResult['replies']
+      self.assetID = assetID
+      for key, value in apiResult.iteritems():
+        setattr(self, key, value)
+    except Exception as e:
+      pdb.set_trace()
+      print apiResult
+      print e
 
-	def __repr__(self):
-		return '<Commentid {}>'.format(self.commentID)
+  def __repr__(self):
+    return '<Commentid {}>'.format(self.commentID)
+
 
 class CommentSchema(Schema):
-		#self links
-	class Meta:
-		type_ = 'comments'
-		fields = ('commentBody','commentID','createDate', 'commentTitle',
-		  'editorsSelection', 'recommendations','userDisplayName', 'userLocation' )
-
-
-
-
+  # self links
+  class Meta:
+    type_ = 'comments'
+    fields = ('commentBody', 'commentID', 'createDate', 'commentTitle',
+              'editorsSelection', 'recommendations', 'userDisplayName', 'userLocation')
--- server/nytimesAPI.py
+++ server/nytimesAPI.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # # New York Times API 
@@ -28,169 +27,172 @@ from models import Article, Comment
 from sqlalchemy.orm import sessionmaker
 import ipdb as pdb
 
-
 # Most popular API
 # Get most popular articles from last 30 days
 
 article_file_name = 'data/most_popular_articles.pkl'
 comment_file_name = 'data/comments.pkl'
-engine = create_engine('postgresql://intuinno:test@localhost/conceptvectorDB',echo=False)
+engine = create_engine('postgresql://postgres:postgres@localhost/conceptvectorDB', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 cache = False
 
 community_keys = [
-					 '5a3d3ff964c9975c0f23d1ad3437dd45:0:70179423',
-					 'affcb9ccf4424a0a94d50af077c653f8:14:71569920',
-					 '5f933be26992203507b0963c96c653f1:4:66447706',
-					 '6adcef7a975045db61389446ca15283e:1:30173638',
-					 '189bb7e323526ce6151defc70519d845:6:70236004',
-					 '403a72d2dad6f7183feea341308946f3:1:72174006',
-					 '5c99af22e7609df06a7168b566699ae4:11:72174619',
-					 '7cd803472503bc5e9fe6dad6ffe56c43:11:72178258',
-					 '0292ebefdcaf75b2fb0d7e7d1404cf09:10:71572358',
-					 '0292ebefdcaf75b2fb0d7e7d1404cf09:10:71572358'	 ]
+  '5a3d3ff964c9975c0f23d1ad3437dd45:0:70179423',
+  'affcb9ccf4424a0a94d50af077c653f8:14:71569920',
+  '5f933be26992203507b0963c96c653f1:4:66447706',
+  '6adcef7a975045db61389446ca15283e:1:30173638',
+  '189bb7e323526ce6151defc70519d845:6:70236004',
+  '403a72d2dad6f7183feea341308946f3:1:72174006',
+  '5c99af22e7609df06a7168b566699ae4:11:72174619',
+  '7cd803472503bc5e9fe6dad6ffe56c43:11:72178258',
+  '0292ebefdcaf75b2fb0d7e7d1404cf09:10:71572358',
+  '0292ebefdcaf75b2fb0d7e7d1404cf09:10:71572358']
 
 currentKey = 0
 
+
 def download_articles():
-	offset = 0
-	apikey = '8ad6d0be3f6e41b7a1ca0c7dbb91bfa5'
+  offset = 0
+  apikey = '8ad6d0be3f6e41b7a1ca0c7dbb91bfa5'
 
-	payload = {'api-key':apikey, 'offset': offset}
-	url = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json'
+  payload = {'api-key': apikey, 'offset': offset}
+  url = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/30.json'
 
-	most_popular_request = requests.get(url, params=payload)
-	num_results = most_popular_request.json()['num_results']
-	most_popular_articles = most_popular_request.json()['results']
+  most_popular_request = requests.get(url, params=payload)
+  num_results = most_popular_request.json()['num_results']
+  most_popular_articles = most_popular_request.json()['results']
 
-	for offset in range(20,num_results,20):
-	    payload = {'api-key':apikey, 'offset': offset}
-	    most_popular_request = requests.get(url, params=payload)
-	    most_popular_articles += most_popular_request.json()['results']
-	    sleep(0.2)
-	    
-	if len(most_popular_articles) != num_results:
-		print "Error: Articles number does not match"
+  for offset in range(20, num_results, 20):
+    payload = {'api-key': apikey, 'offset': offset}
+    most_popular_request = requests.get(url, params=payload)
+    most_popular_articles += most_popular_request.json()['results']
+    sleep(0.2)
 
-	output = file(article_file_name,'wb')
-	pickle.dump(most_popular_articles, output)
+  if len(most_popular_articles) != num_results:
+    print "Error: Articles number does not match"
+
+  output = file(article_file_name, 'wb')
+  pickle.dump(most_popular_articles, output)
+
 
 # Now Add these articles into Database
 
 def getNumComments(a):
+  article_url = a.url
 
-	article_url = a.url
-	
-	try:
-		comment_request = getComments(article_url)
-		num_results = comment_request.json()['results']['totalCommentsFound']
-		return num_results
-	except Exception as e:
-		pdb.set_trace()
-		print e	
-
+  try:
+    comment_request = getComments(article_url)
+    num_results = comment_request.json()['results']['totalCommentsFound']
+    return num_results
+  except Exception as e:
+    # pdb.set_trace()
+    print e
 
 
 def add_articles_database():
+  input = file(article_file_name, 'rb')
+  most_popular_articles = pickle.load(input)
 
-	input = file(article_file_name,'rb')
-	most_popular_articles = pickle.load(input)
-
-	for a in most_popular_articles:
-		if a['id'] > 10000:
-			if session.query(Article).filter_by(id=a['id']).count() == 0:
-				print a['url']
-				aquery = Article(a)
-				try:
-					if getNumComments(aquery) > 1000:
-						session.add(aquery)
-						download_add_comments(a)
-						session.commit()
-				except Exception as e:
-					pdb.set_trace()
-					session.rollback()
-					print a['id']
-					print e
+  for a in most_popular_articles:
+    if a['id'] > 10000:
+      if session.query(Article).filter_by(id=a['id']).count() == 0:
+        print a['url']
+        aquery = Article(a)
+        try:
+          if getNumComments(aquery) > 1000:
+            session.add(aquery)
+            download_add_comments(a)
+            session.commit()
+        except Exception as e:
+          # pdb.set_trace()
+          session.rollback()
+          print a['id']
+          print e
 
 
 def add_replies(all, node, stacklevel):
+  all.append(node)
 
-	all.append(node)
-    
-	for n in node['replies']:
-		add_replies(all, n, stacklevel+1 )
+  for n in node['replies']:
+    add_replies(all, n, stacklevel + 1)
+
 
 def getComments(url, offset=0):
-	global currentKey 
-	payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'depthLimit':100, 'offset':offset}
-	api_url = 'http://api.nytimes.com/svc/community/v3/user-content/url.json'
-	comment_request = requests.get(api_url, params=payload)
-	sleep(0.5)
-	
-	while comment_request.status_code != 200:
-		if comment_request.status_code == 403:
-			currentKey += 1
-			print 'Key inactive: Trying new key', currentKey, comment_request.status_code, url
-			payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset':offset,'depthLimit':100}
-			comment_request = requests.get(api_url, params=payload)
-		elif comment_request.status_code == 504:
-			print 'Gateway Timeout', currentKey, comment_request.status_code, url
-			payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset':offset,'depthLimit':100}
-			comment_request = requests.get(api_url, params=payload)
-		elif comment_request.status_code == 429:
-			currentKey += 1
-			print 'API rate limit exceeded', currentKey, url
-			payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset':offset,'depthLimit':100}
-                        comment_request = requests.get(api_url, params=payload)
-		else: 
-			print 'Not sure why', comment_request.status_code, comment_request.text, url
-			payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset':offset,'depthLimit':100}
-	    	comment_request = requests.get(api_url, params=payload)
-		sleep(0.5)
-	return comment_request
-    
-    
+  global currentKey
+  payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'depthLimit': 100, 'offset': offset}
+  api_url = 'http://api.nytimes.com/svc/community/v3/user-content/url.json'
+  comment_request = requests.get(api_url, params=payload)
+  sleep(0.5)
+
+  while comment_request.status_code != 200:
+    if comment_request.status_code == 403:
+      currentKey += 1
+      print 'Key inactive: Trying new key', currentKey, comment_request.status_code, url
+      payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset': offset,
+                 'depthLimit': 100}
+      comment_request = requests.get(api_url, params=payload)
+    elif comment_request.status_code == 504:
+      print 'Gateway Timeout', currentKey, comment_request.status_code, url
+      payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset': offset,
+                 'depthLimit': 100}
+      comment_request = requests.get(api_url, params=payload)
+    elif comment_request.status_code == 429:
+      currentKey += 1
+      print 'API rate limit exceeded', currentKey, url
+      payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset': offset,
+                 'depthLimit': 100}
+      comment_request = requests.get(api_url, params=payload)
+    else:
+      print 'Not sure why', comment_request.status_code, comment_request.text, url
+      payload = {'api-key': community_keys[currentKey], 'url': url, 'replyLimit': 100, 'offset': offset,
+                 'depthLimit': 100}
+    comment_request = requests.get(api_url, params=payload)
+    sleep(0.5)
+  return comment_request
+
+
 def download_add_comments(a):
-	article_url = a['url']
-	
-	try:
-		comment_request = getComments(article_url)
-		num_parent_results = comment_request.json()['results']['totalParentCommentsFound']
-		num_results = comment_request.json()['results']['totalCommentsFound']
-		comments = comment_request.json()['results']['comments']
-		
-		for offset in range(25,num_parent_results,25):
-		    comment_request = getComments(article_url, offset)
-		    comments += comment_request.json()['results']['comments']
+  article_url = a['url']
 
-		if len(comments) != num_parent_results:
-			print "Error: Parent Comments number does not match",  a['url'], len(comments), num_parent_results
-	
-		all_comments = []
-		for c in comments:
-			add_replies(all_comments, c, 0)
+  try:
+    comment_request = getComments(article_url)
+    num_parent_results = comment_request.json()['results']['totalParentCommentsFound']
+    num_results = comment_request.json()['results']['totalCommentsFound']
+    comments = comment_request.json()['results']['comments']
 
-		if len(all_comments) != num_results:
-			print "Error: All Comments number does not match", a['url'], len(all_comments), num_results
+    for offset in range(25, num_parent_results, 25):
+      comment_request = getComments(article_url, offset)
+      comments += comment_request.json()['results']['comments']
 
-	except Exception as e:
-		pdb.set_trace()
-		print e
+    if len(comments) != num_parent_results:
+      print "Error: Parent Comments number does not match", a['url'], len(comments), num_parent_results
 
-	for c in all_comments:
-		cquery = Comment(c, a['id'])
-		
-		try:			
-			if session.query(Comment).filter_by(commentID=cquery.commentID).count() == 0:
-				session.add(cquery)
-			else:
-				print 'Why Duplicates happend?'
-		except Exception as e:
-			pdb.set_trace()
-			# session.rollback()
-			print a['commentID']
-			print e 
+    all_comments = []
+    for c in comments:
+      add_replies(all_comments, c, 0)
 
-#download_articles()
+    if len(all_comments) != num_results:
+      print "Error: All Comments number does not match", a['url'], len(all_comments), num_results
+
+  except Exception as e:
+    # pdb.set_trace()
+    print e
+
+  for c in all_comments:
+    cquery = Comment(c, a['id'])
+
+    try:
+      if session.query(Comment).filter_by(commentID=cquery.commentID).count() == 0:
+        session.add(cquery)
+      else:
+        print 'Why Duplicates happend?'
+    except Exception as e:
+      # pdb.set_trace()
+      # session.rollback()
+      print a['commentID']
+      print e
+
+
+# download_articles()
 add_articles_database()
